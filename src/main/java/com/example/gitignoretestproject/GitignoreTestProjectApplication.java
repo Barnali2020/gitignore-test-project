3b1306def58fc94d41586615929993dbaec34d61
Barnali <mail2barnali1990@gmail.com>
2020-06-18 12:30:32 +0530

    Project structure

---
+++ src/main/java/com/example/gitignoretestproject/GitignoreTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitignoretestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitignoreTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitignoreTestProjectApplication.class, args);
+	}
+
+}
